91535a9be94a4548ebd0993c85a100d5faabda1f
Kevin Kuek <kevin.kuek.95@gmail.com>
2017-10-17 15:24:13 +0800

    - add unit tests for LCS

---
+++ test_LCS.py
@@ -0,0 +1,48 @@
+from unittest import TestCase
+
+
+class TestLCS(TestCase):
+	def test_doMatching(self):
+		self.fail()
+
+	def test_doesMatch(self):
+		self.fail()
+
+	def test_doCorrectSet(self):
+		self.fail()
+
+	def test_doCovering(self):
+		self.fail()
+
+	def test_updateParameters(self):
+		self.fail()
+
+	def test_consolidateClassifiers(self):
+		self.fail()
+
+	def test_doDeletion(self):
+		self.fail()
+
+	def test_classifyInstance(self):
+		self.fail()
+
+	def test_GA(self):
+		self.fail()
+
+	def test_updateLastGAIterations(self):
+		self.fail()
+
+	def test_selectParents(self):
+		self.fail()
+
+	def test_initialiseChildParameters(self):
+		self.fail()
+
+	def test_doCrossover(self):
+		self.fail()
+
+	def test_doMutation(self):
+		self.fail()
+
+	def test_doSubsumption(self):
+		self.fail()
