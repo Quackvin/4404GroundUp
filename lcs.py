2740fd00fd5d505cd486a85741ca724369ede003
Aaron Hurst <a.hurst.eng@gmail.com>
2017-10-18 21:00:57 +0800

    GA crossover and mutation
    Functions written for GA crossover and mutation along with documentation
    (comments) and required parameters.
    
    No testing completed.

--- lcs.py
+++ lcs.py
@@ -3,18 +3,22 @@ import classifier, random
 class LCS:
 	# '#' indicates wildcard
 	def __init__(self):
-		# put all LCS parameters here
-		self.population = []	# list of Classifier objects
+		# Sets
+		self.population = []
 		self.matchSet = []
 		self.correctSet = []
 
+		# General parameters
+		self.currIter = 0
 		self.maxPopSize = 100
 		self.coveringWildcardProbability = 0.3
+		self.initialRangeFactor = 0.1			# when initialising a rule, set range = initialRangeFactor*centre
 
 		# Parameters for GA
 		self.probabilityCrossover = 0.75
-
-		self.currIter = 0
+		self.probabilityAlleleMutation = 0.02
+		self.probabilityWildcardMutation = 0.2
+		self.mutationScale = 0.1
 
 
 	def doMatching(self, instance):
@@ -37,6 +41,13 @@ class LCS:
 	def doesMatch(self, classifierRules, instanceFeatures):
 		for i in range(len(classifierRules)):
 			# false if not wildcard and outside range from centre
+
+			################################################################################
+			# AARON: I think this is a bug: instanceFeatures refers to the instance from the
+			# environment, which will never be a whilecard. The check needs to be that the
+			# classifier rule is not a wildcard
+			################################################################################
+
 			if instanceFeatures[i] != '#' and \
 					(	instanceFeatures[i] < classifierRules.getLowerBound(i) or
 						instanceFeatures[i] > classifierRules.getUpperBound(i)	):
@@ -62,6 +73,13 @@ class LCS:
 		outcome = instance.outcome
 		rules = classifier.Rules()
 		for feat in instance.features:
+			
+			###########################################################################################
+			# AARON: I think there may be an issue. Does it make sense for the centre to hae a wildcard
+			# but not the range, or vice versa? I.e. you can't do matching if only one is a wildcard.
+			# So I think you should either set both to wildcards, or both to definite values.
+			###########################################################################################
+
 			# add centres
 			if random.randrange(0,100)/100 < self.coveringWildcardProbability:
 				rules.centres.append('#')
@@ -115,12 +133,13 @@ class LCS:
 
 #########################################################################
 
-	def GA(self):
-		# TODO: description
-		# creates 2 new classifiers
-		# applies subsuption to correct set after adding children into it
-		# put matchset and correctset back into population
+	# TASK: description
+	# creates 2 new classifiers
+	# applies subsuption to correct set after adding children into it
+	# put matchset and correctset back into population
 
+	# instanceFeatures is required for mutation back from a wildcard
+	def GA(self, instanceFeatures):
 		# Update last GA iteration for all classifiers in correct set
 		self.updateLastGAIterations()
 
@@ -140,46 +159,129 @@ class LCS:
 		if random.random() < self.probabilityCrossover:
 			(child1, child2) = self.doCrossover(child1, child2)
 
-		# TODO
-		self.doMutation()
-		self.correctSet.append(children[0])
-		self.correctSet.append(children[1])
+		# Apply mutation
+		self.doMutation(child1, instanceFeatures)
+		self.doMutation(child2, instanceFeatures)
+
+		# Add children to correct set
+		self.correctSet.append(child1)
+		self.correctSet.append(child2)
+
+
 		#self.doSubsumption()
 		pass
 	
 
+	# updateLastGAIterations
+	# Called by GA
+	# Loops through correct set and sets lastGAIteration of all elements to currIter
 	def updateLastGAIterations(self):
-		# Called by GA
-		# Loops through correct set and sets lastGAIteration of all elements to currIter
 		for classifier in self.correctSet:
 			classifier.lastGAIteration = self.currIter
 
+	# selectParents
+	# Called by GA
+	# Select a parents classifiers for GA from correct set using Roulette-Wheel Selection.
+	# With Roulette-Wheel Selection, the probability of selecting a given classifier in the
+	# correct set is proportional to its fitness.
 	def selectParent(self):
-		# Select a parents classifier for GA from correct set using Roulette-Wheel Selection
-		# With Roulette-Wheel Selection, the probability of selecting a given classifier in the
-		# correct set is proportional to its fitness.
 		fitnessSum = 0
 		for classifier in self.correctSet:
 			fitnessSum += classifier.fitness
 
-		choicePoint = random.random()*fitnessSum	# select position on wheel
+		choicePoint = random.random()*fitnessSum  # select position on wheel
 		fitnessSum = 0
 		for classifier in self.correctSet:
 			fitnessSum += classifier.fitness
-			if fitnessSum > choicePoint: 	# return classifier at selected position on wheel
+			if fitnessSum > choicePoint:  # return classifier at selected position on wheel
 				return classifier
 
-	def doCrossover(self, childA, childB):
-		# Applies crossover to two children classifiers and returns the modifier children
-		# Uses two point crossover
-		# TODO: include option for single point crossover (third argument or based on an internal parameter)
-		pass
 
-	def doMutation(self):
-		pass
-	# rules in paper
-	# do probabilities here
+	# doCrossover
+	# Called by GA
+	# Applies two-point crossover to two children classifiers and returns the modified children.
+	# Note: crossover is limited to occur only between components of the rule, not between 
+	# alleles. That is, the crossover point will never be between a centre and its corresponding
+	# range value. The crossover point will never split a (centre, range) pair.
+	#
+	# TASK: add option for single point crossover (e.g. extra argument or internal parameter)
+	def doCrossover(self, childA, childB):
+		n_conditions = len(childA.rules.centres)	# number of components in a classifier rule
+		x = random.random()*2*n_conditions			# continuous implementation has two alleles per component
+		y = random.random()*2*n_conditions
+		if x > y:
+			x, y = y, x
+		
+		i = 0
+		while i < y:
+			if x <= i and y > i:
+				childA.rules.centres[i], childB.rules.centres[i] = childB.rules.centres[i], childA.rules.centres[i]
+				childA.rules.rangse[i], childB.rules.ranges[i] = childB.rules.ranges[i], childA.rules.ranges[i]
+			i += 1
+		
+		return (childA, childB)
+	
+
+	# doMutation
+	# Called by GA
+	# Apply mutation to a single classifier. Mutation considers each allele (all centres and
+	# ranges) independently and randomly selects some for mutation based on a set probability.
+	#
+	# Mutation cases:
+	# 1. Non-wildcard to non-wildcard: as per on Sowden (2007) and Stone & Bull (2003), this
+	#    type of mutation is applied by adding an increment in the range (-m,m) to any allele
+	#    selected for such mutation.
+	# 2. Non-wildcard to wildcard: performed with probability probabilityWildcardMutation
+	# 3. Wildcard to non-wildcard: similar to Stone & Bull (2003), this is performed by 
+	#    initialising the rule (centre and range) based on the current environment instance.
+	#    The centre value is calculated by multilying the instance value by a random factor
+	#    close to 1. range is calculated as initialRangeFactor times the centre value.
+	def doMutation(self, child, instanceFeatures):
+		# Mutation of rule centre values
+		for i in range(0,len(child.rules.centres)):
+			# Mutate allele stochastically
+			if random.random() < self.probabilityAlleleMutation:
+				# Wildcard to non-wildcard
+				if child.rules.centres[i] == '#':
+					self.setRuleToRandom(child,instanceFeatures,i)
+				
+				# Non-wildcard to wildcard
+				if random.random() < self.probabilityWildcardMutation:
+					child.rules.centres[i] = '#'
+					child.rules.ranges[i] = '#'
+
+				# Non-wildcard to non-wildcard
+				child.rules.centres[i] += self.mutationScale * random.uniform(-1,1)
+
+		# Mutation of rule range values
+		for i in range(0,len(child.rules.ranges)):
+			# Mutate allele stochastically
+			if random.random() < self.probabilityAlleleMutation:
+				# Wildcard to non-wildcard
+				if child.rules.ranges[i] == '#':
+					self.setRuleToRandom(child,instanceFeatures,i)
+				
+				# Non-wildcard to wildcard
+				if random.random() < self.probabilityWildcardMutation:
+					child.rules.centres[i] = '#'
+					child.rules.ranges[i] = '#'
+
+				# Non-wildcard to non-wildcard
+				child.rules.ranges[i] += self.mutationScale * random.uniform(-1,1)
+
+
+	# setRuleToRandom
+	# Called by doMutation
+	# Initialises a rule (aka predicate or condition) based on the current value in the instance
+	# from the environment. rules.centre is initialised to the value from the environment instance
+	# multiplied by a random factor equal to mutationScale times a random number in (-1,1). 
+	# rules.range is initialised to initialRangeFactor times the centre value.
+	def setRuleToRandom(self, child, instanceFeatures, index):
+		child.rules.centres[index] = instanceFeatures[index] * (1 + self.mutationScale * random.uniform(-1,1))
+		child.rules.ranges[index] = child.rules.centres[index] * self.initialRangeFactor
+
 
 	def doSubsumption(self):
 		pass
 	# applies to correct set
+	
