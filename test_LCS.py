c7743a9f9ddf0ad931c9bac4fe9db1558c7b5729
Kevin Kuek <kevin.kuek.95@gmail.com>
2017-10-21 00:47:42 +0800

    - make test_LCS simpler (need to revert though because proper implementation actually makes more sense)

--- test_LCS.py
+++ test_LCS.py
@@ -1,14 +1,52 @@
 import classifier as classifierModule
 import lcs as lcsModule
+import environment as environmentModule
 
-def test_doesMatch(lcs):
-	testInst = 			[1	,2	,3	,4,	5]
+def test_doMatching():
+	lcs = lcsModule.LCS()
 	testRules = classifierModule.Rules()
-	testRules.centres = ['#',3,	3.4,4,	6]
-	testRules.ranges =  ['#',1,	0.5	,0,	1]
+	testFeats = 			[1, 	2, 		3, 		4, 		-3.1]
+	testRules.centres =		['#',	3,		3.4,	4,		-2]
+	testRules.ranges = 		['#', 	1, 		0.5, 	0, 		1.5]
+
+	testInst = environmentModule.Instance(testFeats, 1)
+
+	testClassifier = classifierModule.Classifier(0, 1, testRules)
+	lcs.population.append(testClassifier)
+
+	testClassifier = classifierModule.Classifier(0, 2, testRules)
+	lcs.population.append(testClassifier)
+
+	print(lcs.doMatching(testInst))
+
+def test_doesMatch():
+	lcs = lcsModule.LCS()
+
+	testRules = classifierModule.Rules()
+	testInst = 				[1	,	2,		3,		4,		-3.1]
+	testRules.centres = 	['#',	3,		3.4,	4,		-2]
+	testRules.ranges =  	['#',	1,		0.5	,	0,		1.5]
 	print(lcs.doesMatch(testRules,testInst))
 
+def test_doCorrectSet():
+	lcs = lcsModule.LCS()
+	testRules = classifierModule.Rules()
+	testFeats = [1, 2, 3, 4, -3.1]
+	testRules.centres = ['#', 3, 3.4, 4, -2]
+	testRules.ranges = ['#', 1, 0.5, 0, 1.5]
 
-lcs = lcsModule.LCS()
+	testInst = environmentModule.Instance(testFeats, 1)
 
-test_doesMatch(lcs)
+	testClassifier = classifierModule.Classifier(0, 1, testRules)
+	lcs.population.append(testClassifier)
+
+	testClassifier = classifierModule.Classifier(0, 2, testRules)
+	lcs.population.append(testClassifier)
+
+	lcs.doMatching(testInst)
+	lcs.doCorrectSet(testInst)
+	print('cSet', len(lcs.correctSet), 'mSet', len(lcs.matchSet), 'pop', len(lcs.population))
+
+test_doMatching()
+test_doesMatch()
+test_doCorrectSet()
