ca92b1c00357652e5066189b2cace87e33fef51d
gyy150 <gyy150@163.com>
2017-10-26 01:53:12 +0800

    “yiyangs testing”

--- main.py
+++ main.py
@@ -3,72 +3,82 @@ import classifier as classifierModule
 import environment
 import json
 
+
 def main(loadPop):
-	env = environment.Environment('dataFull.txt')
-	lcs = lcsModule.LCS()
-	if loadPop:
-		loadPopulation(lcs)
-	run(lcs, env, False)
+    env = environment.Environment('dataFull.txt')
+    lcs = lcsModule.LCS()
+    if loadPop:
+        loadPopulation(lcs)
+    run(lcs, env, False)
+
 
 def run(lcs, env, doTest):
-	# TASK: put this in a while loop so that you complete multiple loops over the 
-	# set of instances
-	for instance in env.instances:
-		lcs.currIter += 1
+    # TASK: put this in a while loop so that you complete multiple loops over the
+    # set of instances
 
-		lcs.doMatching(instance)
-		'''	---NOT IMPLEMENTED YET---'''
-		if(doTest):
-			lcs.formPrediction()
-			'''-------------------'''
-		else:
-			lcs.doCorrectSet(instance)
-			if len(lcs.correctSet) == 0:
-				lcs.doCovering(instance)
-			lcs.updateParameters()
-			if len(lcs.correctSet) > 3: 				# needs more conditions
-				lcs.GA(instance.features)			 	# includes GA subsumption
-			lcs.doCorrectSetSubsumption()
-			lcs.consolidateClassifiers()
-			if len(lcs.population) > lcs.maxPopSize:
-				lcs.doDeletion()
+    maxIteration = 10000
+    while(True):
+        for instance in env.instances:
+            lcs.currIter += 1
 
-			# print(lcs.getAveClassifierAcc())
+            lcs.doMatching(instance)
+            '''	---NOT IMPLEMENTED YET---'''
+            if (doTest):
+                lcs.formPrediction()
+                '''-------------------'''
+            else:
+                lcs.doCorrectSet(instance)
+                if len(lcs.correctSet) == 0:
+                    lcs.doCovering(instance)
+                lcs.updateParameters()
+                if len(lcs.correctSet) > 3:  # needs more conditions
+                    lcs.GA(instance.features)  # includes GA subsumption
+                lcs.doCorrectSetSubsumption()
+                lcs.consolidateClassifiers()
+                if len(lcs.population) > lcs.maxPopSize:
+                    lcs.doDeletion()
 
-		'''---NOT IMPLEMENTED YET---'''
-		endcondition = False
-		'''-------------------------'''
-		if endcondition:
-			savePopulation(lcs.population)
-			return 0
-	savePopulation(lcs.population)
+                # print(lcs.getAveClassifierAcc())
+
+            '''---NOT IMPLEMENTED YET---'''
+            endcondition = lcs.currIter > maxIteration
+            '''-------------------------'''
+            if endcondition:
+                savePopulation(lcs.population)
+                return 0
+
+
+    #savePopulation(lcs.population)
+
 
 def savePopulation(population):
-	with open('classifierPopulation.json', 'w') as writeFile:
-		for classifier in population:
-			classifierDict = classifier.__dict__
-			classifierDict['rules'] = classifierDict['rules'].__dict__
-			classifierString = json.dumps(classifierDict) + '\n'
-			writeFile.write(classifierString)
+    with open('classifierPopulation.json', 'w') as writeFile:
+        for classifier in population:
+            classifierDict = classifier.__dict__
+            classifierDict['rules'] = classifierDict['rules'].__dict__
+            classifierString = json.dumps(classifierDict) + '\n'
+            writeFile.write(classifierString)
+
 
 def loadPopulation(lcs):
-	with open('classifierPopulation.json', 'r') as readFile:
-		for classifierStr in readFile:
-			classifierDict = json.loads(classifierStr)
+    with open('classifierPopulation.json', 'r') as readFile:
+        for classifierStr in readFile:
+            classifierDict = json.loads(classifierStr)
 
-			rules = classifierModule.Rules()
-			rules.centres = classifierDict['rules']['centres']
-			rules.ranges = classifierDict['rules']['ranges']
+            rules = classifierModule.Rules()
+            rules.centres = classifierDict['rules']['centres']
+            rules.ranges = classifierDict['rules']['ranges']
 
-			classifier = classifierModule.Classifier(classifierDict['birthIteration'], classifierDict['outcome'], rules)
-			classifier.matchCount = classifierDict['matchCount']
-			classifier.correctCount = classifierDict['correctCount']
-			classifier.accuracy = classifierDict['accuracy']
-			classifier.fitness = classifierDict['fitness']
-			classifier.numerosity = classifierDict['numerosity']
-			classifier.lastGAIteration = classifierDict['lastGAIteration']
-			classifier.aveMatchSetSize = classifierDict['aveMatchSetSize']
+            classifier = classifierModule.Classifier(classifierDict['birthIteration'], classifierDict['outcome'], rules)
+            classifier.matchCount = classifierDict['matchCount']
+            classifier.correctCount = classifierDict['correctCount']
+            classifier.accuracy = classifierDict['accuracy']
+            classifier.fitness = classifierDict['fitness']
+            classifier.numerosity = classifierDict['numerosity']
+            classifier.lastGAIteration = classifierDict['lastGAIteration']
+            classifier.aveMatchSetSize = classifierDict['aveMatchSetSize']
 
-			lcs.population.append(classifier)
+            lcs.population.append(classifier)
+
 
 main(False)
